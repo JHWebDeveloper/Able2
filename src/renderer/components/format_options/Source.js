fcda2b0bf71f877ebdda56b993decd47f5a0aa29
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2019-09-10 13:56:01 -0400

    added source autocomplete feature

--- src/renderer/components/format_options/Source.js
+++ src/renderer/components/format_options/Source.js
@@ -1,19 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
 import { FormContext } from '../../store/formStore'
 import { updateState, toggleCheckbox } from '../../actions/form'
 
 const Source = () => {
   const { source, sourcePrefix, dispatch  } = useContext(FormContext)
+  const [sourceSuggestions, loadSourceSuggestions] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('https://jhwebdeveloper.github.io/Able2-source-name-suggestions/data.json')
+      .then(res => {
+        loadSourceSuggestions(res.data.sort())
+      })
+  }, [])
+
   return (
     <fieldset name="source-overlay">
       <legend>Source:</legend>
       <input
         type="text"
         name="source"
+        list="source-suggestions"
         value={source}
         onChange={e => dispatch(updateState(e))}
         maxLength="51"
-        placeholder="if none, leave blank"  />
+        placeholder="if none, leave blank" />
+      <datalist id="source-suggestions">
+        {sourceSuggestions.map(src => (
+          <option key={src}>{src}</option>
+        ))}
+      </datalist>
       <label>
         <input
           type="checkbox"

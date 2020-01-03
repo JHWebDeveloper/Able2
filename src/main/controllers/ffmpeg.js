6c5043650c0921162c5c6ccfd7b57c78d5876701
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-01-03 15:58:21 -0500

    removed controllers folder

--- src/main/controllers/ffmpeg.js
+++
@@ -1,9 +0,0 @@
-const ffmpeg = require('fluent-ffmpeg')
-const ffmpegStatic = require('ffmpeg-static-electron')
-const ffprobeStatic = require('ffprobe-static-electron')
-const { fixPathForAsarUnpack } = require('electron-util')
-
-ffmpeg.setFfmpegPath(fixPathForAsarUnpack(ffmpegStatic.path))
-ffmpeg.setFfprobePath(fixPathForAsarUnpack(ffprobeStatic.path))
-
-module.exports = ffmpeg
--- src/main/controllers/getDownloadFormat.js
+++
@@ -1,9 +0,0 @@
-const getDownloadFormat = (out, opt = 'quality') => ([
-  '-f',
-  [
-    opt === 'quality' ? `bestvideo[height<=${out}][fps<=60]+bestaudio/` : '',
-    `best[height<=${out}][fps<=60]/best`
-  ].join('')
-])
-
-module.exports = getDownloadFormat

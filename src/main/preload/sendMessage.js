63fdb95f9e9788206f15c2bc5ffcb4f3113066f6
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-05-01 17:42:09 -0400

    fixed lint error

--- src/main/preload/sendMessage.js
+++ src/main/preload/sendMessage.js
@@ -7,7 +7,6 @@ const sendMessage = ({ sendMsg, recieveMsg, errMsg, data }) => new Promise((reso
   })
 
   ipcRenderer.once(errMsg, (evt, err) => {
-    console.log(err)
     ipcRenderer.removeAllListeners(recieveMsg)
     reject(err)
   })
